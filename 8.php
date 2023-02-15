a774406a781410d0f7ba0ef92f1af57c4a56f03f
Akbar T. Ramadan <113463292+barrrliebert@users.noreply.github.com>
2023-02-15 10:46:12 +0700

    Add files via upload

---
+++ 8.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+?>
+<html>
+<body>
+    <?php
+    $_SESSION["favcolor"] = "yellow";
+    print_r($_SESSION);
+    ?>    
+</body>
+</html>
---
+++ 9.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+?>
+<html>
+<body>
+    <?php
+    session_unset();
+
+    session_destroy();
+    print_r($_SESSION);
+    ?>
+</body>
+</html>
